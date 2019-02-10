2b98653f6f9da6476bf3860a51e8e06a7f87c2bc
Usue <usue@192.168.1.138>
2019-02-10 13:01:14 +0100

    Changes to make CustomSlider inherit from UIView to create the element from every view controller.

--- CustomStepSlider/AppDelegate.swift
+++ CustomStepSlider/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool
     {
-        let mainView = ViewController(steps: ["Step1", "Step2", "Step3", "Step4", "Step5"])
+        let mainView = ViewController()
         
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.rootViewController = mainView
